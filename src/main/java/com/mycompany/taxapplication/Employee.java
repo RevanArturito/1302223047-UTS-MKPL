bf47c77b1161003bc6de415d1b9fc3415782a397
Revan Arturito <revan.arturito@gmail.com>
2025-04-18 19:21:27 +0700

    Refactoring 3 - Primitive Obsession - Employee

--- src/main/java/com/mycompany/taxapplication/Employee.java
+++ src/main/java/com/mycompany/taxapplication/Employee.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class Employee {
 	private String employeeId;	
         private Biodata biodata; 
+        private Spouse spouse;
 	private int yearJoined;
 	private int monthJoined;
 	private int dayJoined;
@@ -27,29 +28,17 @@ public class Employee {
 	private int otherMonthlyIncome;
 	private int annualDeductible;
 	
-	private String spouseName;
-	private String spouseIdNumber;
-
-	private List<String> childNames;
-	private List<String> childIdNumbers;
 	
-	public Employee(String employeeId, Biodata biodata, int yearJoined, int monthJoined, int dayJoined, boolean isForeigner, Gender gender) {
+	public Employee(String employeeId, Biodata biodata, int yearJoined, int monthJoined, int dayJoined,Spouse spouse, boolean isForeigner, Gender gender) {
 		this.employeeId = employeeId;
                 this.biodata = biodata;
+                this.spouse = spouse;
 		this.yearJoined = yearJoined;
 		this.monthJoined = monthJoined;
 		this.dayJoined = dayJoined;
 		this.isForeigner = isForeigner;
 		this.gender = gender;
-		
-		childNames = new LinkedList<String>();
-		childIdNumbers = new LinkedList<String>();
 	}
-	
-	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
-	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
-	 */
 	
 	public void setMonthlySalary(int grade) {	
 		if (grade == 1) {
@@ -79,22 +68,22 @@ public class Employee {
 	}
 	
 	
-	public void addChild(String childName, String childIdNumber) {
-		childNames.add(childName);
-		childIdNumbers.add(childIdNumber);
-	}
+//	public void addChild(String childName, String childIdNumber) {
+//		childNames.add(childName);
+//		childIdNumbers.add(childIdNumber);
+//	}
 	
-	public int getAnnualIncomeTax() {
-		
-		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
-		LocalDate date = LocalDate.now();
-		
-		if (date.getYear() == yearJoined) {
-			monthWorkingInYear = date.getMonthValue() - monthJoined;
-		}else {
-			monthWorkingInYear = 12;
-		}
-		
-		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, spouseIdNumber.equals(""), childIdNumbers.size());
-	}
+//	public int getAnnualIncomeTax() {
+//		
+//		//Menghitung berapa lama pegawai bekerja dalam setahun ini, jika pegawai sudah bekerja dari tahun sebelumnya maka otomatis dianggap 12 bulan.
+//		LocalDate date = LocalDate.now();
+//		
+//		if (date.getYear() == yearJoined) {
+//			monthWorkingInYear = date.getMonthValue() - monthJoined;
+//		}else {
+//			monthWorkingInYear = 12;
+//		}
+//		
+//		return TaxFunction.calculateTax(monthlySalary, otherMonthlyIncome, monthWorkingInYear, annualDeductible, this.spouse.getSpouseIdNumber().equals(""), childIdNumbers.size());
+//	}
 }
