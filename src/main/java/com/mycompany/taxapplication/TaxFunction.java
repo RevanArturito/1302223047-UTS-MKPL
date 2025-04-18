b03ebd62e1dd9a9512c4a6569ba895caca3ed48b
Revan Arturito <revan.arturito@gmail.com>
2025-04-18 21:22:51 +0700

    Refactoring 9 - Long Method - Calculate Tax

--- src/main/java/com/mycompany/taxapplication/TaxFunction.java
+++ src/main/java/com/mycompany/taxapplication/TaxFunction.java
@@ -9,29 +9,21 @@ package com.mycompany.taxapplication;
  * @author User
  */
 public class TaxFunction {
-//    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
-//		
-//        int tax = 0;
-//
-//        if (numberOfMonthWorking > 12) {
-//                System.err.println("More than 12 month working per year");
-//        }
-//
-//        if (numberOfChildren > 3) {
-//                numberOfChildren = 3;
-//        }
-//
-//        if (isMarried) {
-//                tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - (54000000 + 4500000 + (numberOfChildren * 1500000))));
-//        }else {
-//                tax = (int) Math.round(0.05 * (((monthlySalary + otherMonthlyIncome) * numberOfMonthWorking) - deductible - 54000000));
-//        }
-//
-//        if (tax < 0) {
-//                return 0;
-//        }else {
-//                return tax;
-//        }
-//
-//    }
+    public static int calculateTax(int monthlySalary, int otherMonthlyIncome, int numberOfMonthWorking, int deductible, boolean isMarried, int numberOfChildren) {
+        if (numberOfMonthWorking > 12) {
+            throw new IllegalArgumentException("More than 12 month working per year");
+        }
+
+        int childrenCount = numberOfChildren > 3 ? TaxConstants.MAX_CHILDREN : numberOfChildren;
+
+        int yearlyIncome = (monthlySalary + otherMonthlyIncome) * numberOfMonthWorking;
+        int nonTaxable = TaxConstants.NOT_MARRIED_TAX;
+
+        if (isMarried) {
+            nonTaxable += TaxConstants.CHILD_TAX + (childrenCount * TaxConstants.CHILD_TAX);
+        }
+
+        int taxableIncome = yearlyIncome - deductible - nonTaxable;
+        return Math.max(0, (int)Math.round(taxableIncome * TaxConstants.TAX_RATE));
+    }
 }
